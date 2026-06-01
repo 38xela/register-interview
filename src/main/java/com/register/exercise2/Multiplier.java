170bb3140d44f2828af3e57aeaf847d3e74c8a5c
Alessandro Cosimati <alessandro_cosimati@msn.com>
2026-06-01 11:06:25 +0200

    added more use case for multiply method

--- src/main/java/com/register/exercise2/Multiplier.java
+++ src/main/java/com/register/exercise2/Multiplier.java
@@ -1,7 +1,15 @@
 package com.register.exercise2;
 
 public class Multiplier {
-    public static int[] multiply(int first, int second) {
-        return null;
+    public static int[] multiply(int a, int b) {
+        return addNTimes(BigNumber.toDigits(a), b);
+    }
+
+    private static int[] addNTimes(int[] big, int n) {
+        int[] result = new int[]{0};
+        for (int i = 0; i < n; i++) {
+            result = BigNumber.add(result, big);
+        }
+        return result;
     }
 }
--- src/test/com/register/exercise2/MultiplierTest.java
+++ src/test/com/register/exercise2/MultiplierTest.java
@@ -9,4 +9,21 @@ class MultiplierTest {
     void multiply_exerciseExample_15times2returns30() {
         assertArrayEquals(new int[]{0, 3}, Multiplier.multiply(15, 2));
     }
+
+    @Test
+    void multiply_byZero_returnsZero() {
+        assertArrayEquals(new int[]{0}, Multiplier.multiply(5, 0));
+    }
+
+    @Test
+    void multiply_byOne_returnsOriginalNumber() {
+        assertArrayEquals(new int[]{7}, Multiplier.multiply(7, 1));
+    }
+
+    @Test
+    void multiply_isCommutative() {
+        assertArrayEquals(Multiplier.multiply(3, 4), Multiplier.multiply(4, 3));
+    }
+
+    
 }
