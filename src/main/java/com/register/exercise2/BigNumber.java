4f20a6249f08ef3ace67c52655e85b2e5d9144db
Alessandro Cosimati <alessandro_cosimati@msn.com>
2026-06-01 10:42:00 +0200

    added toDigit method

--- src/main/java/com/register/exercise2/BigNumber.java
+++ src/main/java/com/register/exercise2/BigNumber.java
@@ -1,5 +1,19 @@
 package com.register.exercise2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class BigNumber {
 
+    public static int[] toDigits(int n) {
+        if (n == 0) return new int[]{0};
+
+        List<Integer> digits = new ArrayList<>();
+        while (n > 0) {
+            digits.add(n % 10);  // % naturally gives LSB first
+            n /= 10;
+        }
+        return digits.stream().mapToInt(Integer::intValue).toArray();
+    }
 }
--- src/test/com/register/exercise2/BigNumberTest.java
+++ src/test/com/register/exercise2/BigNumberTest.java
@@ -9,4 +9,14 @@ class BigNumberTest {
     void toDigits_singleDigit_returnsSingleElementArray() {
         assertArrayEquals(new int[]{5}, BigNumber.toDigits(5));
     }
+
+    @Test
+    void toDigits_multiDigit_returnsLsbFirst() {
+        assertArrayEquals(new int[]{3, 2, 1}, BigNumber.toDigits(123));
+    }
+
+    @Test
+    void toDigits_zero_returnsSingleZero() {
+        assertArrayEquals(new int[]{0}, BigNumber.toDigits(0));
+    }
 }
